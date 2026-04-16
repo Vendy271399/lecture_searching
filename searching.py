0fdc46e9a1bd63ebd6de603c407fbfb18d5fbfff
Vendy271399 <271399@vutbr.cz>
2026-04-16 15:40:41 +0200

    version_04

--- searching.py
+++ searching.py
@@ -12,6 +12,24 @@ def read_data(file_name, field):
         return None
 
 
+def linear_search(sequential_data, find_number):
+    dict_data = {}
+    position = []
+
+    for i, number in enumerate(sequential_data):
+        if number == find_number:
+            position.append(i)
+
+    counter = sequential_data.count(find_number)
+
+    dict_data["positions "] = position
+    dict_data["count"] = counter
+
+    return dict_data
+
+
+
+
 
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
